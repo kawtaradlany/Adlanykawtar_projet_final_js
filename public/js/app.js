5bcf5aaca8b2b2df60c4a4b58c457a7df6fc97c3
kawtaradlany <adlanikawtar@gmail.com>
2023-12-26 16:25:12 +0100

    mail-first commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,22 @@
-let first = prompt("chose what u need : sign-up, login, forget password");
+let first = prompt("chose what u need : 7 sign-up, login, forget password");
 
-if (first == "sign-up") {
-    let fullName = prompt("enter your FullName")
-    while (fullName.length < 5) {
-        fullName = prompt("enter your FullName")
-    }
-    if (fullName.match("regex")) {
-        fullName = prompt("enter your FullName")
-    }
+if (first == "7") {
+    // let fullName = prompt("enter your FullName")
+    // while (fullName.length < 5) {
+    //     fullName = prompt("enter your FullName")
+    // }
     
-    console.log(fullName.trim().charAt(0).toUpperCase() + fullName.slice(1).replace(/[^\w ]/g, ''));
+    // if (fullName.match("regex")) {
+    //     fullName = prompt("enter your FullName")
+    // }
+
+    // console.log(fullName.trim().charAt(0).toUpperCase() + fullName.slice(1).replace(/[^\w ]/g, ''));
+    let mail = prompt("insert your mail!")
+    if (mail.indexOf(` `)) {
+        mail = prompt("enleve l'espace")
+    }
+   
+
+    console.log(mail.trim().toLowerCase());
 
 }
