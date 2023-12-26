659cb7656686b4570d58823755b5ca10a91e9979
kawtaradlany <adlanikawtar@gmail.com>
2023-12-26 15:09:30 +0100

    login-name

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,14 @@
 let first = prompt("chose what u need : sign-up, login, forget password");
+
 if (first == "sign-up") {
     let fullName = prompt("enter your FullName")
     while (fullName.length < 5) {
         fullName = prompt("enter your FullName")
     }
+    if (fullName.match("regex")) {
+        fullName = prompt("enter your FullName")
+    }
     
-    
-    console.log(fullName.trim().charAt(0).toUpperCase() + fullName.slice(1));
+    console.log(fullName.trim().charAt(0).toUpperCase() + fullName.slice(1).replace(/[^\w ]/g, ''));
 
 }
