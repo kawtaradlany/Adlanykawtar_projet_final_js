420fce685e4f5181d6095ef3503ce995bb222fdc
kawtaradlany <adlanikawtar@gmail.com>
2023-12-26 14:15:53 +0100

    name/first while

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
-let first= prompt("chose what u need : signe up, login, forget password");
+let first = prompt("chose what u need : sign-up, login, forget password");
+if (first == "sign-up") {
+    let fullName = prompt("enter your FullName")
+    while (fullName.length < 5) {
+        fullName = prompt("enter your FullName")
+    }
+    
+    
+    console.log(fullName.trim().charAt(0).toUpperCase() + fullName.slice(1));
+
+}
